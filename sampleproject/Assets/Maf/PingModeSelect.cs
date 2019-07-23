9a5de4aa1cf2fb427099f0531049bde6f3eabb1d
Maf Callow <Mathewc@mesmerise.it>
2019-07-23 16:44:20 +0100

    Added mode select

---
+++ sampleproject/Assets/Maf/PingModeSelect.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PingModeSelect : MonoBehaviour
+{
+    private void OnGUI()
+    {
+        if (GUILayout.Button("Server"))
+        {
+            SceneManager.LoadScene("PingServer");
+        }
+        if (GUILayout.Button("Client"))
+        {
+            SceneManager.LoadScene("PingClient");
+        }
+    }
+}
